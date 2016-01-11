c0beb6d161dc4f5fec944b9b8d7c88b143e67bfa
Mothilde <vivou_kurt@hotmail.fr>
2016-01-11 16:54:29 +0100

    exo

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello worls!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'no'."
+puts 'I "said" do not touch this.'
+
---
+++ ex3.rb
@@ -0,0 +1,21 @@
+put "I will now count my chickens:"
+
+put "Hens#{25+30/6}"
+puts "Roosters #{100-25*3%4}"
+
+puts 3+2+1-5+4%261/4+6
+
+puts "Is it true that 3+2<5-7?"
+
+puts 3+2<5-7
+
+puts "What is 3+2? #{3+2}"
+puts "What is 5-7? #{5-7}"
+
+puts "Oh, that's why it's false.""
+
+puts "How about some more."
+
+puts "Is it greater?#{5> -2}"
+puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5 <= -2}"
